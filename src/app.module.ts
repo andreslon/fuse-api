1cae6f77943bf3da1064eb0f76e525f37a8f40ef
Andrés Londoño <andreslon@outlook.com>
2025-05-21 00:58:06 -0500

    update dependencies in package.json and package-lock.json; remove unused AppController and AppService; integrate Swagger for API documentation in main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
+import { RootModule } from './root/root.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HealthModule, RootModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fuse API')
+    .setDescription('The Fuse API description')
+    .setVersion('1.0')
+    .addTag('fuse')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
+  console.log(`Swagger documentation is available at: http://localhost:${process.env.PORT ?? 3000}/api`);
 }
 bootstrap();
